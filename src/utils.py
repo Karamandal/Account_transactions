88b126b96df26baa5e0235de59697da0cfc248a7
Karamandal <158753111+Karamandal@users.noreply.github.com>
2024-02-18 03:01:09 +0700

    Исправил путь до исходных данных (#5)

--- src/utils.py
+++ src/utils.py
@@ -1,36 +1,42 @@
 import json
+import os
 from datetime import datetime
 
-# Открываем файл JSON
-with open('Z:\projects\Diplom\Account transactions\data\operations.json', encoding='utf-8') as file:
-    """"Выставить путь до данных через папку дата"""
-    # Загружаем содержимое файла в переменную
-    data = json.load(file)
 
+def print_first_5_transactions():
+    # Открываем файл JSON
+    with open(os.path.join("..", "data", "operations.json"), encoding='utf-8') as file:
+        # Загружаем содержимое файла в переменную
+        data = json.load(file)
 
-def get_transaction():
+    # Выводим первые 5 транзакций
     for transaction in data:
-        return transaction
+        print(f'{format_operation(transaction)}\n')
 
 
 def format_operation(transaction):
-    # Получаем дату из строки и преобразуем в нужный формат
-    date = datetime.strptime(transaction['date'], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+    if transaction != {}:
+        # Получаем дату из строки и преобразуем в нужный формат
+        date = datetime.strptime(transaction['date'], "%Y-%m-%dT%H:%M:%S.%f").strftime("%d.%m.%Y")
+        # Получаем остальные данные операции
+        description = transaction['description']
+        to_ = ' '.join(transaction['to'].split(' ')[:-1])
+        account = transaction['to'].split(' ')[-1]
+        amount = transaction['operationAmount']['amount'] + ' ' + transaction['operationAmount']['currency']['name']
+        if description != "Открытие вклада":
+            from_ = ' '.join(transaction['from'].split(' ')[:-1])
+            card = transaction['from'].split(' ')[-1]
+            # Формируем отформатированную строку
+            if from_ == 'Счет':
+                formatted_operation = f'{date} {description}\n{from_} {card[0:4]} {card[4:8]} {card[8:10]}** **** {card[16:]} - {to_} **{account[-4:]}\n{amount}'
+            else:
+                formatted_operation = f'{date} {description}\n{from_} {card[0:5]} {card[5:7]}** **** {card[12:]} - {to_} **{account[-4:]}\n{amount}'
+            return formatted_operation
+        else:
+            to_ = ' '.join(transaction['to'].split(' ')[:-1])
+            # Формируем отформатированную строку
+            formatted_operation = f'{date} {description}\n{to_} **{account[-4:]}\n{amount}'
+            return formatted_operation
 
-    # Получаем остальные данные операции
-    description = transaction['description']
-    from_ = ' '.join(transaction['from'].split(' ')[:-1])
-    card = transaction['from'].split(' ')[-1]
-    to_ = ' '.join(transaction['to'].split(' ')[:-1])
-    account = transaction['to'].split(' ')[-1]
-    amount = transaction['operationAmount']['amount'] + ' ' + transaction['operationAmount']['currency']['name']
-    # Формируем отформатированную строку
-    if from_ == 'Счет':
-        formatted_operation = f'{date} {description}\n{from_} {card[0:4]} {card[4:8]} {card[8:10]}** **** {card[16:]} - {to_} **{account[-4:]}\n{amount}'
-    else:
-        formatted_operation = f'{date} {description}\n{from_} {card[0:5]} {card[5:7]}** **** {card[12:]} - {to_} **{account[-4:]}\n{amount}'
 
-    return formatted_operation
-
-
-print(format_operation(get_transaction()))
+print_first_5_transactions()
